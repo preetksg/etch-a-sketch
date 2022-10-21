d79f8695dc53b1f05b25ea5b600dfe26be4c5ead
Preet <preetgrewal2444@gmail.com>
2022-10-20 19:15:10 -0600

    resets correctly now

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const container = document.querySelector('.container');
 
-// Add 16 divs
-
 function createGrid(squares) {
     for (let i = 0; i < (squares*squares); i++) {
         const div = document.createElement('div');
         div.classList.add('grid-item');
         container.appendChild(div);
     }
+    container.style.gridTemplateColumns="repeat("+squares+",auto)";
     hoverColor();
 }
 
@@ -21,13 +20,25 @@ function hoverColor () {
   }
 
   function input () {
-    let squares = prompt("Enter number of squares on each side");
-    return squares;
+    //reset color
+    container.innerText='';
+    let items = document.querySelectorAll('.grid-item');
+    items.forEach(item => {
+        item.style.backgroundColor = 'white';
+      });
+    let squares = prompt("Enter number of squares on each side, max is 100.");
+    if ((squares > 0) && (squares < 101)) {}
+    else {
+        squares = 0;
+    }
+    return(squares);
   }
 
   const btn1 = document.getElementById('btn1');
   btn1.addEventListener('click', () => {
     let squares = input();
     createGrid(squares);
-    container.style.gridTemplateColumns="repeat("+squares+",auto [col-start])";
+    //container.style.gridTemplateColumns="repeat("+squares+",auto [col-start])";
+    console.log(squares)
+    container.removeChild(div);
 } )
